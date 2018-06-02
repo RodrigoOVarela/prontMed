b49443ff5d0720c95145b7a55dafba845777d5f5
Rodrigo <rodrigooliveiravarela@gmail.com>
2018-06-02 14:00:15 -0300

    Adicionado projeto ao git

---
+++ src/pages/create-account/create-account.ts
@@ -0,0 +1,43 @@
+import { UsersProvider } from './../../providers/users/users';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-create-account',
+  templateUrl: 'create-account.html',
+})
+export class CreateAccountPage {
+  model: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, private userProvider: UsersProvider) {
+    this.model = new User();
+    this.model.name = 'Rodrigo';
+    this.model.lastName = 'Varela';
+    this.model.phone = 49999555789;
+    this.model.email = 'rodrigo@gmail.com';
+    this.model.password = 'teste';
+    this.model.repeatPassword = 'teste';
+    }
+
+  createAccount() {
+    this.userProvider.createAccount(this.model.email, this.model.password)
+      .then((result: any) => {
+        this.toast.create({ message: 'Usuário criado com sucesso. Token: ' + result.token, position: 'botton', duration: 3000 }).present();
+
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao criar o usuário. Erro: ' + error.error, position: 'botton', duration: 3000 }).present();
+      });
+  }
+}
+
+export class User {
+  name: string;
+  lastName: string;
+  phone: number;
+  email: string;
+  password: string;
+  repeatPassword: string;
+
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { UsersProvider } from './../../providers/users/users';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  model: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, private userProvider: UsersProvider) {
+    this.model = new User();
+    this.model.email = 'rodrigo@gmail.com';
+    this.model.password = 'teste';
+  }
+
+  login() {
+    this.userProvider.login(this.model.email, this.model.password)
+      .then((result: any) => {
+        this.toast.create({ message: 'Usuário logado com sucesso. Token: ' + result.token, position: 'botton', duration: 3000 }).present();
+
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao efetuar login. Erro: ' + error.error, position: 'botton', duration: 3000 }).present();
+      });
+  }
+}
+
+export class User {
+  email: string;
+  password: string;
+}
